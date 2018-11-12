d67cc1452aa1bb24cae3c2be84b0dd8a8c5daeed
Soumalya Dey <Soumalya_Dey@epam.com>
2018-11-12 23:06:06 +0100

    Added Sanity check test

--- randomDice/src/test/java/com/nordea/codetest/randomDice/RandomDiceApplicationTests.java
+++ randomDice/src/test/java/com/nordea/codetest/randomDice/RandomDiceApplicationTests.java
@@ -1,16 +1,25 @@
 package com.nordea.codetest.randomDice;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.nordea.codetest.randomDice.controller.RandomDiceController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RandomDiceApplicationTests {
 
+	@Autowired
+	private RandomDiceController controller;
+
 	@Test
-	public void contextLoads() {
+	public void testContextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
