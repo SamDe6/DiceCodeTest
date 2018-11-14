8bca275ce72f09da6d5cdbf917a5fe518fed1467
Soumalya Dey <Soumalya_Dey@epam.com>
2018-11-14 10:51:05 +0100

    Removed redundant annotation

--- randomDice/src/main/java/com/nordea/codetest/randomDice/repository/DiceRepository.java
+++ randomDice/src/main/java/com/nordea/codetest/randomDice/repository/DiceRepository.java
@@ -3,11 +3,9 @@ package com.nordea.codetest.randomDice.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.nordea.codetest.randomDice.model.Dice;
 
-@Repository
 public interface DiceRepository extends CrudRepository<Dice, Long> {
 
 	<S extends Dice> S save(Dice dice);
